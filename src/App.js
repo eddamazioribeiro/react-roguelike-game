8eda5a08e531bbc4016458ce081156cc68f3c5e0
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-12-15 22:44:55 -0300

    Created game canvas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import ReactRogue from './ReactRogue';
 
-function App() {
+const App =  () => {
   return (
     <div className="App">
       <h2>Welcome to React Roguelike Game</h2>
       <hr></hr>
       <h4>(Under development)</h4>
+      <div>
+        <ReactRogue/>
+      </div>
     </div>
   );
 }
---
+++ src/ReactRogue.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ReactRogue =  () => {
+  return (
+    <canvas
+      width="256"
+      height="256"
+      style={{border: '1px solid black'}}>
+
+    </canvas>
+  );
+}
+
+export default ReactRogue;
