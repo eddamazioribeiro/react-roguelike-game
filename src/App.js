ae2849568b4366d17134d14d31065fcc9615271a
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-12-15 22:31:03 -0300

    Initial commit: created project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to React Roguelike Game</h2>
+      <hr></hr>
+      <h4>(Under development)</h4>
+    </div>
+  );
+}
+
+export default App;
